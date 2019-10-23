5f4ffa63e08f3cb26acaf81d3b624f1568825f34
suna <pooyo314@dongguk.edu>
2019-10-23 16:04:22 +0900

    add requirements.txt

---
+++ chatbot/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+
+# JSON
+from django.http import JsonResponse
+import json
+# CSRF
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def webhook(request):
+    if request.method == 'POST':
+        req = json.loads(request.body)
+
+        action = req.get('queryResult').get('action')
+        params = req.get('queryResult').get('parameters')
+
+        if action == 'self_introduce':
+            returns self_introduce()
+        elif action = 'create':
+            return create()    
+
+
+def self_introduce():
+    response = {
+        'fulfillmentText': '안녕하세요, 저는 해외직구 반품을 도와주는 돌직구 입니다!'
+    }
+    return JsonResponse(response, safe = False)
+    #오류를 피하기 위해 safe = False를 해준다.
+
+def create():
